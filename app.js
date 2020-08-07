845b039305b103b079ea2426fd526854507397f8
Will <22464726+willgleich@users.noreply.github.com>
2020-08-06 21:35:21 -0600

    Blog Overhaul, Image update and resume polish. Kustomize replica update

--- app.js
+++ app.js
@@ -20,7 +20,25 @@ app.get('/index', function(req, res){
 
 
 app.get('/blog', function(req, res){
-    var data1 = {title: "Coming soon to a browser near you...", body:"First Blog Post"};
+    var data1 = {title: "Homelab Antics",
+                body:"            <!-- Post Content -->\n" +
+                    "            <p class=\"lead\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>\n" +
+                    "\n" +
+                    "            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>\n" +
+                    "\n" +
+                    "            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>\n" +
+                    "\n" +
+                    "            <blockquote class=\"blockquote\">\n" +
+                    "                <p class=\"mb-0\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>\n" +
+                    "                <footer class=\"blockquote-footer\">Someone famous in\n" +
+                    "                    <cite title=\"Source Title\">Source Title</cite>\n" +
+                    "                </footer>\n" +
+                    "            </blockquote>\n" +
+                    "\n" +
+                    "            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>\n" +
+                    "\n" +
+                    "            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>\n",
+                date: "2020-08-08"};
     res.render('blog', {data:data1});
 });
 
