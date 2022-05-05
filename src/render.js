1510553ed5a6ddf547cbc9b69753b464ba0b74d9
Will <22464726+willgleich@users.noreply.github.com>
2022-05-04 22:05:49 -0600

    Add ejs render.js

---
+++ src/render.js
@@ -0,0 +1,96 @@
+const util = require("util");
+const fs = require("fs")
+const ejs = require("ejs")
+const path = require('path');
+const blog = require("./blog.js");
+
+
+const mkdir = util.promisify(fs.mkdir);
+// const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+
+async function render(filename) {
+    let file_name = path.posix.basename(filename, ".ejs")
+
+    switch (file_name) {
+        case "base":
+            return;
+        default:
+            var render_data = {}
+        case "blog":
+            var blog_data = await blog.get_blog_data();
+            var render_data = {"data":blog_data.reverse()}
+    }
+    // file_name = file_path.
+    try {
+        //create output directory
+        await mkdir("dist", { recursive: true });
+
+        //render ejs template to html string
+        //pass pageModel in to render content
+        const html = await ejs
+            .renderFile(filename, render_data)
+            .then((output) => output);
+        //create file and write html
+        if (file_name === "index") {
+            var output_file = "dist/" + file_name + ".html"
+        }
+        else {
+            var output_file = "dist/" + file_name
+        }
+        console.log(filename)
+        await writeFile(output_file, html);
+    } catch (error) {
+        console.log("test")
+        console.log(error);
+    }
+}
+
+// function render(filename) {
+//     console.log(filename)
+//     file_name = path.posix.basename(filename, ".ejs")
+//     // file_name = file_path.
+//     try {
+//         //create output directory
+//         mkdir("dist", { recursive: true });
+//
+//         //render ejs template to html string
+//         //pass pageModel in to render content
+//         const html =  ejs
+//             .renderFile(filename, {})
+//             .then((output) => output);
+//         //create file and write html
+//         output_file = "dist/" + file_name + ".html"
+//         fs.writeFile(output_file, html, (err) => {
+//             if (err)
+//                 console.log(err);
+//             else {
+//                 console.log("File written successfully\n");
+//                 console.log("The written has the following contents:");
+//                 console.log(fs.readFileSync("books.txt", "utf8"));
+//             }
+//     })
+//     } catch (error) {
+//         console.log("test")
+//         console.log(error);
+//     }
+// }
+
+const filenames = []
+fs.readdirSync("views").forEach(file => {
+    filenames.push("views/"+file)
+});
+
+
+
+// const filenames = ["views/index.ejs", "views/aboutme.ejs", "views/resume.ejs"]
+
+
+Promise.all(filenames.map(render)).then(() => { console.log('all done!'); })
+// for (let item of filenames) {
+//     render(item).then(() => {
+//        console.log("Successfully rendered " + item)
+//     });
+// }
+
